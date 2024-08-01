2bb0a0fb16057c0f723fc54136fc8964dc05eaf8
iwaura masato <miwaura95@gmail.com>
2024-08-01 11:51:04 +0200

    feature: edit table

--- flask-version/metagenomongo/static/index.js
+++ flask-version/metagenomongo/static/index.js
@@ -1,8 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Select all <td> elements within the .table-responsive divs
-    document.querySelectorAll('.table-responsive th').forEach(function(th) {
-        if (th.innerHTML == "collected_by" || th.innerHTML == "comment"){
-            th.classList.add('th-width');
-        }
-    });
+window.addEventListener('load', function () {
+	document.querySelectorAll('table td').forEach(elm => {
+		elm.contentEditable = true;
+	});
 });
