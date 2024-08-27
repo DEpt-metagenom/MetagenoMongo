8983946b47845457435a4f1f14e7ada45e11cc8d
iwaura masato <miwaura95@gmail.com>
2024-08-27 11:27:43 +0200

    fix: edit email message and handle multiple email address

--- flask-version/metagenomongo/module/email.py
+++ flask-version/metagenomongo/module/email.py
@@ -8,15 +8,20 @@ def send_email(file_name, remote_path):
 
     sender_email = os.getenv('META_EMAIL')
     recipient_email = os.getenv('RECIPIENT_EMAIL')
-    recipient_email = recipient_email.split(',')
+    try:
+        recipient_email = recipient_email.split(',')
+        print(recipient_email)
+    except:
+        print(f"An error occurred: check RECIPIENT_EMAIL is set correctly")
+        return
     sender_password = os.getenv('META_PASS')
     message = MIMEMultipart()
     message['Subject'] = f"The file {file_name} upload"
     message['From'] = sender_email
-    message['To'] = recipient_email
+    
 
     # Add body to email
-    message.attach(MIMEText(f"The file {file_name} has been uploaded to the {remote_path}.", 'plain', "utf-8"))
+    message.attach(MIMEText(f"The file {file_name} has been uploaded to the {remote_path}.", 'plain'))
 
     # Create SMTP session
     try:
@@ -28,8 +33,12 @@ def send_email(file_name, remote_path):
         # Send the email
         text = message.as_string()
         for recipient in recipient_email:
-            server.sendmail(sender_email, recipient, text)
-        print("Email sent successfully!")
+            try:
+                message['To'] = recipient
+                server.sendmail(sender_email, recipient, text)
+                print(f"Email sent successfully to {recipient}!")
+            except Exception as e:
+                print(f"An error occurred while sending the email to {recipient}: {e}")
 
     except Exception as e:
         print(f"An error occurred while sending the email: {e}")
