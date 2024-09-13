aa71ef491df6ddb2f19949deca3946519fbdd0d3
iwaura masato <miwaura95@gmail.com>
2024-09-13 15:24:34 +0200

    fix: remove one of the two user_name input field

--- flask-version/metagenomongo/static/index.js
+++ flask-version/metagenomongo/static/index.js
@@ -15,4 +15,10 @@ function setupButton(selector, type, value, color = null, onClick = null) {
 window.addEventListener('load', function () {
   setupButton('input[name="Delete"]', 'button', 'X', 'red');
   setupButton('input[name="Duplicate"]', 'button', 'Dupl');
+  const user_name1 = document.getElementById('user_name1');
+  const user_name2 = document.getElementById('user_name2');
+  user_name1.addEventListener('input', function() {
+    const inputValue = user_name1.value;
+    user_name2.value = inputValue;
+  })
 });
